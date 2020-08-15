870e2da6e197a6abb8ba3cbf09809f2707efcc7d
Nicola Gasparro <nicola.gasp11@gmail.com>
2020-08-15 19:19:47 +0200

    react app todoc checklist

---
+++ todo-app/src/App.js
@@ -0,0 +1,96 @@
+import React, { useState, useCallback, useEffect } from 'react';
+
+
+/* function App() {
+  return (
+    <div>
+      <h1>Hello World!!</h1>
+    </div>
+  );
+} */
+
+const App = () => {
+  const [newTodo, setNewTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const onNewTodoChange = useCallback((event) => {
+        setNewTodo(event.target.value);
+  }, []);
+
+  const formSubmitted = useCallback((event) => {
+    event.preventDefault();
+    if (!newTodo.trim()) return;
+    setTodos([
+      {
+        id: todos.length ? todos[0].id + 1 : 1,
+        content: newTodo,
+        done: false,
+      }, 
+      ...todos
+    ]);
+    setNewTodo('');
+  }, [newTodo, todos]);
+
+  useEffect(() => {
+    console.log('todos', todos);
+  }, [todos]);
+
+  const addTodo = useCallback((todo, index ) => (event) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1, {
+      ...todo,
+      done: !todo.done
+    });
+    setTodos(newTodos);
+  }, [todos]);
+  
+
+  const removeTodo = useCallback((todo) => (event) => {
+    setTodos(todos.filter(otherTodo => otherTodo !== todo));
+  }, [todos]);
+
+  const markAllDone = useCallback(() => {
+    // create a copy of the array 
+      // create a copy of each of the items 
+       // update the done proprerty to be true of the new items 
+    const updatedTodos = todos.map(todo => {
+      return {
+        ...todo, 
+        done: true,
+      };
+    });
+    setTodos(updatedTodos);
+  }, [todos]);
+  
+  return (
+    <div>
+        <form onSubmit={formSubmitted}>
+          <label htmlFor="newTodo">Enter a Todo : </label>
+          <input 
+            id="newTodo"
+            name="newTodo"
+            value={newTodo} 
+            onChange={onNewTodoChange}>
+          </input>
+        <button>Add Todo</button>
+        </form>
+        <button onClick={markAllDone}>Mark all Done</button>
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={todo.id}>
+              <input 
+                checked={todo.done}
+                type="checkbox"
+                onChange={addTodo(todo, index)}
+              >
+              </input>
+              <span className={todo.done ? 'done' : ''}>{todo.content}</span>
+              <button onClick={removeTodo(todo)}>Remove Todo</button>
+            </li>
+          ))}
+        </ul>
+    </div>
+  );
+};
+
+export default App;
